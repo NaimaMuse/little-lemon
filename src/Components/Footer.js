61e66f835a203cf09b4ff3437a26b0e2db10335e
NaimaMuse <Naciimaaahmed@gmail.com>
2026-09-11 11:33:47 +0300

    Create semantic React components

---
+++ src/Components/Footer.js
@@ -0,0 +1,33 @@
+function Footer() {
+  return (
+    <footer>
+      <h2>Little Lemon</h2>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+
+          <li>
+            <a href="/about">About</a>
+          </li>
+
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+        </ul>
+      </nav>
+
+      <p>Chicago, Illinois</p>
+
+      <p>© 2026 Little Lemon</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <main>
+      <h1>Welcome to Little Lemon</h1>
+      <p>
+        We are a family-owned Mediterranean restaurant in Chicago.
+      </p>
+    </main>
+  );
+}
+
+export default Main;
